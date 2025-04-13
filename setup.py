13d00f210891cf5a7a06860985854f0486b41f8d
Minyus <Minyus@github.com>
2025-04-13 21:13:55 +0800

    Release 0.0.2

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ https://github.com/Minyus/g-sheet-x
 
 setup(
     name="gsheetx",
-    version="0.0.1",
+    version="0.0.2",
     packages=find_packages(exclude=["tests"]),
     entry_points={"console_scripts": console_scripts},
     install_requires=requires,
